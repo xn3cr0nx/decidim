1918293919d5ea558a48217b4ba1359e97cc0a1c
Agustí B.R <hola@agusti.cat>
2020-07-06 16:15:01 +0200

    chore(projects): use the budgets resource (public)

--- decidim-budgets/app/cells/decidim/budgets/project_list_item_cell.rb
+++ decidim-budgets/app/cells/decidim/budgets/project_list_item_cell.rb
@@ -37,10 +37,14 @@ module Decidim
         render
       end
 
+      def voting_finished?
+        !current_settings.votes_enabled? && current_settings.show_votes?
+      end
+
       private
 
       def resource_path
-        resource_locator(model).path
+        resource_locator([model.budget, model]).path
       end
 
       def resource_title
